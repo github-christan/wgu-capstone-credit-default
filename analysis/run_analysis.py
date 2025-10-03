128702ef49bcc58b990c4c7af84bb5a2128aa4d7
Christopher Tan <tan.chris.v@gmail.com>
2025-10-03 15:33:13 -0400

    Rollback SQL Alchemy for analysis

--- analysis/run_analysis.py
+++ analysis/run_analysis.py
@@ -1,23 +1,23 @@
 import os
 import pandas as pd
-from sqlalchemy import create_engine
-from snowflake.sqlalchemy import URL
+from snowflake.connector import connect
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score, accuracy_score, confusion_matrix
 
-engine = create_engine(URL(
-    account=os.environ["SNOWFLAKE_ACCOUNT"],
-    user=os.environ["SNOWFLAKE_USER"],
-    password=os.environ["SNOWFLAKE_PASSWORD"],
-    role=os.environ["SNOWFLAKE_ROLE"],
+# Use the same credentials you used for dbt (hardcode or better: set environment variables)
+conn = connect(
+    account=os.environ.get("SNOWFLAKE_ACCOUNT", "SLQAGZU-ORC07712"),
+    user=os.environ.get("SNOWFLAKE_USER", "CTAN"),
+    password=os.environ.get("SNOWFLAKE_PASSWORD", "L@bingL@bing01"),
+    role=os.environ.get("SNOWFLAKE_ROLE", "ACCOUNTADMIN"),
     warehouse=os.environ.get("SNOWFLAKE_WAREHOUSE", "WH_DEV"),
     database="CREDIT_DEFAULT",
     schema="MODEL",
-))
+)
 
-df = pd.read_sql("SELECT * FROM FEATURE_VIEW_CREDIT_DEFAULT", engine)
-engine.dispose()
+df = pd.read_sql("SELECT * FROM FEATURE_VIEW_CREDIT_DEFAULT", conn)
+conn.close()
 
 y = df["TARGET"].astype(int)
 X = df.drop(columns=["TARGET"])
